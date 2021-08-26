986a3bfbc976f6ca7baa4e9973056aef6f7d6202
MarcosEMachado <68864709+MarcosEMachado@users.noreply.github.com>
2021-08-25 22:46:01 -0300

    141

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,19 +1,28 @@
 import { select, call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess, updateAmountReserve } from './actions';
+import { addReserveSuccess, updateAmountSuccess } from './actions';
 import api from '../../../services/api';
 
-function* addToReserve({ id }){
+function* addToReserve({ id }) {
   const tripExists = yield select(
     state => state.reserve.find(trip => trip.id === id)
   );
 
-  if(tripExists){ 
-    const amount = tripExists.amount + 1;
+  const myStock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = myStock.data.amount;
+  const currentStock = tripExists ? tripExists.amount : 0;
+  const amount = currentStock + 1;
 
-    yield put(updateAmountReserve(id, amount));
+  if (amount > stockAmount) {
+    alert('Sem estoque!!!');
+    return;
+  }
 
-  }else{
-    const response = yield call(api.get, `trips/${id}` );
+  if (tripExists) {
+
+    yield put(updateAmountSuccess(id, amount));
+
+  } else {
+    const response = yield call(api.get, `trips/${id}`);
 
     const data = {
       ...response.data,
@@ -22,11 +31,22 @@ function* addToReserve({ id }){
 
     yield put(addReserveSuccess(data))
   }
+}
 
+function* updateAmount({ id, amount }){
+  if(amount <= 0) return;
+  const myStock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = myStock.data.amount;
+  if (amount > stockAmount) {
+    alert('Sem estoque!!!');
+    return;
+  }
 
+  yield put(updateAmountSuccess(id, amount));
 
 }
 
 export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
-])
+  takeLatest('ADD_RESERVE_REQUEST', addToReserve),
+  takeLatest('UPDATE_RESERVE_REQUEST', updateAmount)
+]);
